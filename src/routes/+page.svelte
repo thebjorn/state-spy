1015c273b8b7effb2e74c3ba2c2b120557e7a96f
thebjorn <bp@datakortet.no>
2024-02-21 18:54:15 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+    import StateSpy from '$lib/StateSpy.svelte';
+    let {data} = $props();
+</script>
+
+<StateSpy data={data} />
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
